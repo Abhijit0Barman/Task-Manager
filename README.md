8e39bfebea49141b265a1bc1c5eb5930a79ece71
Abhijit0barman <abhijitbarman96@gmail.com>
2024-03-23 19:14:29 +0530

    "Added try-catch blocks for error handling in user controller, updated README with routes"
    This commit includes the following changes:
    
    1. Added try-catch blocks for error handling in the `handleGetAllUsers`, `handleCreateNewUser`, `handleLogin`, `handleLogout` functions in `user.controller.js`. This will ensure that the server responds with an appropriate error message instead of crashing in case of an error.
    2. Updated the `README.md` file with the list of routes for the application. This will make it easier for users to understand the different endpoints available in the application.

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Task-Manager
+
+#Routes
+https://task-manager-virt.onrender.com ---->Default Route(SSR) 
+https://task-manager-virt.onrender.com/refresh ---->Refresh Token
+https://task-manager-virt.onrender.com/api/users ------>GET ALL USERS
+https://task-manager-virt.onrender.com/api/users/register
+https://task-manager-virt.onrender.com/api/users/login
+https://task-manager-virt.onrender.com/api/users/logout
+https://task-manager-virt.onrender.com/api/users/:id ------>GET/PATCH/DELETE
+https://task-manager-virt.onrender.com/api/tasks  -------->POST/(GET ALL TASK)
+https://task-manager-virt.onrender.com/api/tasks/:id  -------->GET/PATCH/DELETE
+
+
--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -5,133 +5,134 @@ const { BlacklistModel } = require("../models/blacklist.model");
 require("dotenv").config();
 
 async function handleGetAllUsers(req, res) {
-  const allDbUsers = await UserModel.find({});
-  return res.send(allDbUsers);
+    try {
+        const allDbUsers = await UserModel.find({});
+        return res.send(allDbUsers);
+    } catch (error) {
+        res.status(401).send({ error: error.message });
+    }
 }
 
 const handleGetUserById = async (req, res) => {
-  const id = req.params.id;
-  const user = await UserModel.findById(id);
-  // const user = users.find((user) => user.id === id);
-  if (!user) return res.status(404).json({ err: "Does Not Exist" });
-  return res.json(user);
+    const id = req.params.id;
+    const user = await UserModel.findById(id);
+    if (!user) return res.status(404).json({ err: "Does Not Exist" });
+    return res.json({ msg: "User Profile", user });
 };
 
 const handleUpdateUserById = async (req, res) => {
-  const id = req.params.id;
-  const user = await UserModel.findByIdAndUpdate(id, {
-    lastName: "patched",
-  });
-  // const user = users.find((user) => user.id === id);
-  return res.json(user);
+    const id = req.params.id;
+    const user = await UserModel.findByIdAndUpdate(id, req.body);
+
+    return res.json({ msg: "Update Completed", user });
 };
 
 async function handleDeleteUserById(req, res) {
-  const id = req.params.id;
-  // const user = users.find((user) => user.id === id);
-  await UserModel.findByIdAndDelete(id);
-  return res.json(user);
+    const id = req.params.id;
+
+    await UserModel.findByIdAndDelete(id);
+    return res.json({ msg: "Deleted Successfully" });
 }
 
 const handleCreateNewUser = async (req, res) => {
-  const body = req.body;
-  const { username, email, password } = body;
-  if (!body || !password || !email || !username) {
-    return res.status(400).json({ error: "All Fields is Required" });
-  }
-
-  try {
-    const user = await UserModel.findOne({ email });
-    if (user) {
-      return res.status(200).send({ message: "User Already Exists" });
+    const body = req.body;
+    const { username, email, password } = body;
+    if (!body || !password || !email || !username) {
+        return res.status(400).json({ error: "All Fields is Required" });
     }
-    bcrypt.hash(password, 5, async function (err, hash) {
-      if (err) {
-        res.status(200).send({
-          error: err.message,
+
+    try {
+        const user = await UserModel.findOne({ email });
+        if (user) {
+            return res.status(200).send({ message: "User Already Exists" });
+        }
+        bcrypt.hash(password, 5, async function (err, hash) {
+            if (err) {
+                res.status(200).send({
+                    error: err.message,
+                });
+            } else {
+                const user = new UserModel({
+                    username,
+                    email,
+                    password: hash,
+                });
+                await user.save();
+                res.status(201).send({
+                    msg: "User Registered Successfully",
+                    registered_mail: email,
+                    id: user._id,
+                });
+            }
         });
-      } else {
-        const user = new UserModel({
-          username,
-          email,
-          password: hash,
-        });
-        await user.save();
-        res.status(201).send({
-          msg: "User Registered Successfully",
-          registered_mail: email,
-          id: user._id,
-        });
-      }
-    });
-  } catch (error) {
-    res.status(400).send({ msg: error.message });
-  }
+    } catch (error) {
+        res.status(400).send({ msg: error.message });
+    }
 };
 
 const handleLogin = async (req, res) => {
-  const { email, password } = req.body;
-  try {
-    const user = await UserModel.findOne({ email });
-    if (user) {
-      bcrypt.compare(password, user.password, function (err, result) {
-        if (err) {
-          res.status(201).send({ error: err.message });
-        }
-        // result == true
-        if (result) {
-          const token = jwt.sign(
-            { userID: user._id, username: user.username },
-            process.env.SECRET_KEY,
-            {
-              expiresIn: "7d",
-            }
-          );
+    const { email, password } = req.body;
+    try {
+        const user = await UserModel.findOne({ email });
+        if (user) {
+            bcrypt.compare(password, user.password, function (err, result) {
+                if (err) {
+                    res.status(201).send({ error: err.message });
+                }
+                // result == true
+                if (result) {
+                    const token = jwt.sign(
+                        { userID: user._id, username: user.username },
+                        process.env.SECRET_KEY,
+                        {
+                            expiresIn: "7d",
+                        }
+                    );
 
-          const refresh_token = jwt.sign(
-            { email: email },
-            process.env.REFRESH_TOKEN_SECRET,
-            { expiresIn: "28d" }
-          );
+                    const refresh_token = jwt.sign(
+                        { email: email },
+                        process.env.REFRESH_TOKEN_SECRET,
+                        { expiresIn: "28d" }
+                    );
 
-          res.status(201).send({
-            msg: "User Logged in Successfully",
-            token: token,
-            refresh_token,
-          });
+                    res.status(201).send({
+                        msg: "User Logged in Successfully",
+                        token: token,
+                        refresh_token,
+                    });
+                } else {
+                    res.status(201).send({ msg: "Wrong Password!" });
+                }
+            });
         } else {
-          res.status(201).send({ msg: "Wrong Password!" });
+            res.status(201).send({
+                msg: "User does not exist, Please Register!",
+            });
         }
-      });
-    } else {
-      res.status(201).send({
-        msg: "User does not exist, Please Register!",
-      });
+    } catch (error) {
+        res.status(401).send({ error: error.message });
     }
-  } catch (error) {
-    res.status(401).send({ error: error.message });
-  }
 };
 
 const handleLogout = async (req, res) => {
-  const token = req.headers.authorization?.split(" ")[1];
-  try {
-    const blackToken = new BlacklistModel({ token });
-    await blackToken.save();
-    res.status(201).send({
-      msg: "User Logout successfully",
-    });
-  } catch (error) {
-    res.status(401).send({ error: error.message });
-  }
+    const token = req.headers.authorization?.split(" ")[1];
+    try {
+        const blackToken = new BlacklistModel({ token });
+        await blackToken.save();
+        res.status(201).send({
+            msg: "User Logout successfully",
+        });
+    } catch (error) {
+        res.status(401).send({ error: error.message });
+    }
 };
 
 module.exports = {
-  handleGetAllUsers,
-  handleGetUserById,
-  handleUpdateUserById,
-  handleDeleteUserById,
-  handleCreateNewUser,
-  handleLogin,
-  handleLogout,
+    handleGetAllUsers,
+    handleGetUserById,
+    handleUpdateUserById,
+    handleDeleteUserById,
+    handleCreateNewUser,
+    handleLogin,
+    handleLogout,
 };
