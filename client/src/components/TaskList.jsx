ab9b8720bc3061b845a0b9cff6ef0b76aa53e2bd
Abhijit0barman <abhijitbarman96@gmail.com>
2024-03-24 06:00:22 +0530

    delete function also completed

--- client/src/components/TaskList.jsx
+++ client/src/components/TaskList.jsx
@@ -1,10 +1,11 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, shallowEqual, useSelector } from "react-redux";
 import { getAllTask } from "./../redux/taskReducer/action";
+import axios from "axios";
 
-export const TaskList = ({ren}) => {
+export const TaskList = ({ ren }) => {
     const dispatch = useDispatch();
-
+    const [render, setRender] = useState(false);
     const { tasks, isLoading, token } = useSelector((store) => {
         return {
             tasks: store.taskReducer.tasks,
@@ -38,19 +39,49 @@ export const TaskList = ({ren}) => {
     useEffect(() => {
         dispatch(getAllTask(token));
         // getTask();
-    }, [ren]);
+    }, [ren,render]);
+
+    const handleDelete = (id) => {
+        axios
+            .delete(`https://task-manager-virt.onrender.com/api/tasks/${id}`, {
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${token}`,
+                },
+            })
+            .then((res) => {
+                console.log(res.data);
+                setRender(p=>!p)
+            })
+            .catch((err) => console.log(err));
+    };
+    // const handleEdit = (id) => {};
 
     return (
         <div className="bg-[#5734E4] m-auto text-center rounded p-3 w-[500px] max-h-dvh">
             <h1 className="text-white font-bold text-center text-[20px]">
-                Add Task
+                Task List
             </h1>
             {isLoading && <p>Loading...</p>}
             <ul>
-                {tasks?.map((item) => (
+                {tasks.length>0 && tasks?.map((item) => (
                     <li className="m-[20px] bg-slate-50" key={item._id}>
                         <h2>Title: {item.title}</h2>
                         <p>Description: {item.description}</p>
+                        <div className="flex gap-6 items-start justify-around py-3">
+                            <button
+                                onClick={() => handleEdit(item._id)}
+                                className="bg-[blue] w-20 p-1 text-white hover:bg-[yellow] hover:text-[black]"
+                            >
+                                Edit
+                            </button>
+                            <button
+                                onClick={() => handleDelete(item._id)}
+                                className="bg-[red] w-20 p-1 text-white hover:bg-[yellow] hover:text-[black]"
+                            >
+                                Delete
+                            </button>
+                        </div>
                     </li>
                 ))}
             </ul>
